0db17d2d44ab4e682b70d83a35c3ec30438b9141
MattCSmith <mrmcchow@gmail.com>
2018-10-03 21:44:04 +0100

    Include head and menu via JS

---
+++ js/headFoot.js
@@ -0,0 +1,74 @@
+document.getElementById('head').innerHTML =
+    '<meta charset="utf-8">' +
+    '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">' +
+    '<title>ZTM Halloween - Hacktoberfest</title>' +
+    '<link href="https://fonts.googleapis.com/css?family=Griffy" rel="stylesheet">' +
+    '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">' +
+    '<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>' +
+    '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>' +
+    '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>' +
+    '<!-- fontawesome cdn-->' +
+    '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">' +
+    '<!-- Custom styles for this template -->' +
+    '<link href="./css/carousel.css" rel="stylesheet">' +
+    '<link href="./css/style.css" rel="stylesheet">' +
+    '<!-- meta icon -->' +
+    '<link rel="icon" href="./images/pumpkin.jpg" type="image/gif" sizes="32x32">'
+
+
+
+
+
+
+document.getElementById("myNav").innerHTML =
+
+    '<header>' +
+        '<nav class="navbar navbar-expand-md navbar-dark fixed-top">' +
+
+        
+
+        '<a class="navbar-brand" href="index.html"><img src="images/ztm-halloween-logo.svg"/></a>' +
+        '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">' +
+          '<span class="navbar-toggler-icon"></span>' +
+        '</button>' +
+        '<div class="collapse navbar-collapse" id="navbarCollapse">' +
+          '<ul class="navbar-nav mr-auto" id="linsks">' +
+
+          
+            
+            '<li class="nav-item active">' +
+              '<a class="nav-link" href="index.html">Home<span class="sr-only">(current)</span></a>' +
+            '</li>' +
+
+            
+            '<li class="nav-item">' +
+              '<a class="nav-link" href="recipes.html">Recipes</a>' +
+            '</li>' +
+
+            
+            '<li class="nav-item">' +
+              '<a class="nav-link" href="facts.html">Facts</a>' +
+            '</li>' +
+
+            
+            '<li class="nav-item">' +
+              '<a class="nav-link" href="designs.html">Pumpkin Designs</a>' +
+            '</li>' +
+
+            
+            '<li class="nav-item">' +
+              '<a class="nav-link" href="names.html">Names</a>' +
+            '</li>' +
+            
+            
+            '<li class="nav-item">' +
+                '<a class="nav-link" href="halloweenMovies.html">Movies</a>' +
+              '</li>' +
+
+          '</ul>'+
+        '</div>' +
+      '</nav>' +
+    '</header>' 
+
+
+
