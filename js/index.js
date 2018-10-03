bcdb10bf76dc3f9269309e939d92da7badce27f8
Jess <jessdev83@gmail.com>
2018-10-03 19:52:37 -0400

    Navbar Color on Scroll
    Added css class and javascript functionality to make navbar background requested color on scroll. Color does not show when navbar is at the top of the page.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // modal on load
-$(window).on('load',function(){
+$(window).on('load', function() {
     $('#exampleModalCenter').modal('show');
     //console.log('ready111');
 });
@@ -8,11 +8,11 @@ let links = document.querySelectorAll('.nav-link');
 // let start = window.location.href.lastIndexOf('/')+1;
 let url = window.location.href;
 // console.log(url);
-links.forEach((link)=>{
-	if(link.href === url)	
-		link.parentElement.classList.add('active');
-	else 
-		link.parentElement.classList.remove('active');
+links.forEach((link) => {
+    if (link.href === url)
+        link.parentElement.classList.add('active');
+    else
+        link.parentElement.classList.remove('active');
 })
 
 // Disabled for testing
@@ -21,35 +21,40 @@ links.forEach((link)=>{
 //     if (state == 'interactive') {
 //          document.getElementById('contents').style.visibility="hidden";
 //     } else if (state == 'complete') {
-        
+
 // //            document.getElementById('interactive');
 //            document.getElementById('load').style.visibility="hidden";
 //            //document.getElementById('exampleModalCenter').style.visibility = "hidden";
 //            document.getElementById('contents').style.visibility="visible";
-        
+
 //     }
 //   }
 
-  $(document).ready(function(){
-    $(window).scroll(function () {
-           if ($(this).scrollTop() > 50) {
-               $('#back-to-top').fadeIn();
-           } else {
-               $('#back-to-top').fadeOut();
-           }
-       });
-       // scroll body to 0px on click
-       $('#back-to-top').click(function () {
-           $('#back-to-top').tooltip('hide');
-           $('body,html').animate({
-               scrollTop: 0
-           }, 800);
-           return false;
-       });
-       
-       $('#back-to-top').tooltip('show');
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function() {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    $('#back-to-top').tooltip('show');
 
 });
 
-
-
+//Add color to navbar on scroll and remove color when back to top.
+document.addEventListener('scroll', () => {
+    document.querySelector('.navbar').classList.add('navbar-scroll');
+    if (window.scrollY === 0) {
+        document.querySelector('.navbar').classList.remove('navbar-scroll');
+    }
+})
