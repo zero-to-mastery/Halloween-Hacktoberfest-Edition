aaca6856b170234b3ca3dd7f4c4aae7de972f67b
sb39 <biswassrijeet99@gmail.com>
2018-10-04 18:30:27 +0530

    minor fixes

--- js/headFoot.js
+++ js/headFoot.js
@@ -110,8 +110,8 @@ document.getElementById("endScripts").innerHTML =
     
     '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>' +
     
-    '<script src="./js/global.js"></script>' +
-    esContents
+    '<script src="./js/global.js"></script>' +esContents
+    
 
 //Add color to navbar on scroll and remove color when back to top.
 document.addEventListener('scroll', () => {
@@ -120,3 +120,4 @@ document.addEventListener('scroll', () => {
       document.querySelector('.navbar').classList.remove('navbar-scroll');
   }
 })
+
