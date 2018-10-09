09532f5205ef47630275f198bd879d9402d6bb0f
Kamil Swider <kamil.b.swider@gmail.com>
2018-10-09 20:15:50 +0200

    added games tab

--- js/headFoot.js
+++ js/headFoot.js
@@ -54,6 +54,11 @@ document.getElementById("myNav").innerHTML =
               '<a class="nav-link" href="halloweenBooks.html">Books</a>' +
           '</li>' +
 
+          '<!-- Games Link -->' +
+          '<li class="nav-item">' +
+              '<a class="nav-link" href="halloweenGames.html">Games</a>' +
+          '</li>' +
+
           '<!-- Catch the Witch Link -->' +
           '<li class="nav-item">' +
               '<a class="nav-link" href="catchTheWitch.html">Catch the Witch</a>' +
