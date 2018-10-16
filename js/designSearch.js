1dd76fc6a2ebfc36e815155ade2668bd89cf22c6
Manisha Awasthi <itsmanisha_15@yahoo.co.in>
2018-10-16 23:15:26 +0100

    removed console log

--- js/designSearch.js
+++ js/designSearch.js
@@ -4,9 +4,8 @@
 
  const designs = document.getElementsByClassName("card");
  for (i = 0; i < designs.length; i++) {
-        a = designs[i].getElementsByTagName('h3');
-        console.log(a[0].innerText);
-        if (a[0].innerHTML.toUpperCase().indexOf(filter) > -1) {
+        card = designs[i].getElementsByTagName('h3');
+        if (card[0].innerHTML.toUpperCase().indexOf(filter) > -1) {
             designs[i].style.display = "";
         } else {
             designs[i].style.display = "none";
