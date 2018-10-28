6372c90d36ce1d04712fb735f54f85cc8e4c5028
Lei_tiger <leifu@oregonstate.edu>
2018-10-28 16:40:27 -0700

    changed font and color for activities page

--- js/cssLoad.js
+++ js/cssLoad.js
@@ -8,6 +8,7 @@ document.getElementById('head').innerHTML =
         '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">' +
         '<title>ZTM Halloween - Hacktoberfest</title>' +
         '<link href="https://fonts.googleapis.com/css?family=Griffy" rel="stylesheet">' +
+        '<link href="https://fonts.googleapis.com/css?family=Mali" rel="stylesheet"> '+
         '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">' +
         '<!-- fontawesome cdn-->' +
         '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">' +
@@ -22,6 +23,7 @@ document.getElementById('head').innerHTML =
         '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">' +
         '<title>ZTM Halloween - Hacktoberfest</title>' +
         '<link href="https://fonts.googleapis.com/css?family=Griffy" rel="stylesheet">' +
+        '<link href="https://fonts.googleapis.com/css?family=Mali" rel="stylesheet"> '+
         '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">' +
         '<!-- fontawesome cdn-->' +
         '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">' +
